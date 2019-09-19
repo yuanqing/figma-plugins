5e909c403054146e90cc89d17846ac5559c19488
yuanqing <desk@yuanqing.sg>
2019-09-18 23:40:16 -0400

    Edit `prettier` command

--- packages/figma-prettier/src/commands/prettier.js
+++ packages/figma-prettier/src/commands/prettier.js
@@ -2,16 +2,9 @@ import cleanNode from '../utilities/clean-node'
 
 export default function (figma) {
   const selection = figma.currentPage.selection
-  if (selection.length > 0) {
-    for (const node of selection) {
-      cleanNode(node)
-    }
-  } else {
-    for (const pageNode of figma.root.children) {
-      for (const node of pageNode.children) {
-        cleanNode(node)
-      }
-    }
+  const nodes = selection.length > 0 ? selection : figma.root.children
+  for (const node of nodes) {
+    cleanNode(node)
   }
   figma.closePlugin()
 }
