d2cc4bd6bd0865ea8b3f1cbbd3dbd3cf30b0e0a2
yuanqing <desk@yuanqing.sg>
2019-09-23 00:08:12 -0400

    Setup `preact`

--- create-figma-plugin.config.js
+++ create-figma-plugin.config.js
@@ -1,5 +1,23 @@
 module.exports = function (config) {
   config.module.rules.push({
+    test: /\.js?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        plugins: [
+          [
+            '@babel/plugin-transform-react-jsx',
+            {
+              pragma: 'h',
+              pragmaFrag: 'Fragment'
+            }
+          ]
+        ]
+      }
+    }
+  })
+  config.module.rules.push({
     test: /\.css$/,
     use: [
       {
