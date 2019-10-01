3de7d6878eca25a769b21c84eea57845982b2331
yuanqing <desk@yuanqing.sg>
2019-10-01 23:23:37 +0800

    Allow translating text layers with multiple lines

--- packages/figma-language-tester/src/translate.js
+++ packages/figma-language-tester/src/translate.js
@@ -2,12 +2,25 @@ import { fetch } from '@create-figma-plugin/utilities'
 import pMemoize from 'p-memoize'
 
 export const translate = pMemoize(async function (text, languageKey) {
-  const result = await fetch(
+  const json = await fetch(
     `
-      https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${languageKey}&dt=t&q=${encodeURI(
+      https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${languageKey}&dt=t&q=${encode(
       text
     )}
     `
   )
-  return result[0][0][0]
+  if (json[0].length === 1) {
+    return json[0][0][0]
+  }
+  const result = []
+  json[0].forEach(function (line) {
+    result.push(line[0])
+  })
+  return result.join('')
 })
+
+const newlineRegex = /\n/
+
+function encode (text) {
+  return encodeURI(text).replace(newlineRegex, '%0A%0A')
+}
