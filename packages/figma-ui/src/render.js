a73b4ef90e52dacdd811252c568335dfde7990ee
yuanqing <desk@yuanqing.sg>
2019-10-04 07:35:58 +0800

    Remove Tachyons

--- packages/figma-ui/src/render.js
+++ packages/figma-ui/src/render.js
@@ -1,7 +1,6 @@
 /** @jsx h */
 import { h, render as preactRender } from 'preact'
 import 'figma-plugin-ds/figma-plugin-ds.min.css'
-import 'tachyons/css/tachyons.min.css'
 
 export function render (Plugin) {
   return function (rootNode, data) {
