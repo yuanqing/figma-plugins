59a06ba57db0748538420b03e5e9fbc30c15d169
yuanqing <desk@yuanqing.sg>
2019-10-04 21:56:49 +0800

    Allow setting `padding`

---
+++ packages/figma-draw-slice-over-selection/src/command.js
@@ -0,0 +1,39 @@
+/* global figma */
+import { calculateMaximumBounds } from './calculate-maximum-bounds'
+import {
+  addCommandEventListener,
+  loadSettings,
+  saveSettings,
+  showUi
+} from '@create-figma-plugin/utilities'
+
+export default async function () {
+  const selection = figma.currentPage.selection
+  if (selection.length === 0) {
+    figma.closePlugin('Select one or more layers')
+    return
+  }
+  const settings = (await loadSettings()) || {
+    padding: 0
+  }
+  addCommandEventListener('DRAW_SLICE_OVER_SELECTION', async function (
+    settings
+  ) {
+    await saveSettings(settings)
+    const { padding } = settings
+    const maximumBounds = calculateMaximumBounds(selection)
+    const slice = figma.createSlice()
+    const width = maximumBounds[1].x - maximumBounds[0].x + 2 * padding
+    const height = maximumBounds[1].y - maximumBounds[0].y + 2 * padding
+    slice.x = maximumBounds[0].x - padding
+    slice.y = maximumBounds[0].y - padding
+    slice.resize(width, height)
+    slice.name = '@SliceOverSelection'
+    slice.locked = true
+    figma.closePlugin('✔ Drew slice over selection')
+  })
+  addCommandEventListener('CANCEL', async function () {
+    figma.closePlugin()
+  })
+  showUi({ width: 240, height: 100, data: settings })
+}
---
+++ packages/figma-draw-slice-over-selection/src/components/app.js
@@ -0,0 +1,41 @@
+/** @jsx h */
+import { triggerCommandEvent } from '@create-figma-plugin/utilities'
+import { Button, InputWithIcon, useForm } from 'figma-ui'
+import { h } from 'preact'
+import './app.scss'
+
+export function App (initialState) {
+  function submitCallback ({ padding }) {
+    triggerCommandEvent('DRAW_SLICE_OVER_SELECTION', {
+      padding: parseFloat(padding)
+    })
+  }
+  function cancelCallback () {
+    triggerCommandEvent('CANCEL')
+  }
+  const { inputs, handleInputChange, handleSubmit } = useForm(
+    initialState,
+    submitCallback,
+    cancelCallback
+  )
+  return (
+    <div class='app'>
+      <div class='app__input'>
+        <InputWithIcon
+          type='number'
+          iconColor='black-3'
+          iconName='group'
+          name='padding'
+          onKeyUp={handleInputChange}
+          value={inputs.padding}
+          focused
+        />
+      </div>
+      <div class='app__button'>
+        <Button type='primary' onClick={handleSubmit}>
+          Draw Slice Over Selection
+        </Button>
+      </div>
+    </div>
+  )
+}
