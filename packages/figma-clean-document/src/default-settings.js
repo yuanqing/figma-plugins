587b7e6d15d6f6773c0fb2f5deb791c6fa0603d2
yuanqing <desk@yuanqing.sg>
2019-10-14 01:03:09 +0800

    Add `scope`

--- packages/figma-clean-document/src/default-settings.js
+++ packages/figma-clean-document/src/default-settings.js
@@ -1,5 +1,8 @@
+import { DOCUMENT } from './clean-document/scope'
+
 export const defaultSettings = {
   deleteHiddenLayers: true,
+  scope: DOCUMENT,
   sortPages: true,
   smartSortLayers: true,
   smartRenameLayers: true,
