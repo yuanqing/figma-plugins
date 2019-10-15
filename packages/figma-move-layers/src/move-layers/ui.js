f8fdb3d5b1da0fa42fea98e1514a96675c4c17c4
yuanqing <desk@yuanqing.sg>
2019-10-15 20:07:31 +0800

    Move `components` folder

--- packages/figma-move-layers/src/move-layers/ui.js
+++ packages/figma-move-layers/src/move-layers/ui.js
@@ -1,4 +1,4 @@
 import { render } from 'figma-ui/src/render'
-import { MoveLayers } from '../components/move-layers'
+import { MoveLayers } from './components/move-layers'
 
 export default render(MoveLayers)
