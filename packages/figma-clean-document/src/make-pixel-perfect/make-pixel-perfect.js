04a06ef5dce8adacf7aee0b839dbfe3ea1a51958
yuanqing <desk@yuanqing.sg>
2019-10-20 21:06:05 +0800

    Fix logic `makePixelPerfect`

--- packages/figma-clean-document/src/make-pixel-perfect/make-pixel-perfect.js
+++ packages/figma-clean-document/src/make-pixel-perfect/make-pixel-perfect.js
@@ -1,15 +1,22 @@
-const ATTRIBUTES = ['height', 'width', 'x', 'y']
-
 export function makePixelPerfect (layer) {
-  if (layer.type === 'GROUP') {
+  if (layer.type === 'GROUP' || layer.type === 'VECTOR') {
     return false
   }
   let didChange = false
-  for (const attribute of ATTRIBUTES) {
-    if (Number.isInteger(layer[attribute]) === false) {
-      layer[attribute] = Math.round(layer[attribute])
-      didChange = true
-    }
+  if (Number.isInteger(layer.x) === false) {
+    layer.x = Math.round(layer.x)
+    didChange = true
+  }
+  if (Number.isInteger(layer.y) === false) {
+    layer.y = Math.round(layer.y)
+    didChange = true
+  }
+  if (
+    Number.isInteger(layer.width) === false ||
+    Number.isInteger(layer.height) === false
+  ) {
+    layer.resize(Math.round(layer.width), Math.round(layer.height))
+    didChange = true
   }
   return didChange
 }
