fdd1aea519fa4f6d46649e52a4c722b182378882
yuanqing <desk@yuanqing.sg>
2019-11-18 00:27:19 +0800

    Migrate to `@create-figma-plugin/ui`

--- packages/figma-draw-slice-over-selection/src/draw-slice-over-selection/components/draw-slice-over-selection.js
+++ packages/figma-draw-slice-over-selection/src/draw-slice-over-selection/components/draw-slice-over-selection.js
@@ -1,10 +1,11 @@
 /** @jsx h */
 import { triggerEvent } from '@create-figma-plugin/utilities'
-import { Button } from 'figma-ui/src/components/button'
-import { InputWithIcon } from 'figma-ui/src/components/input-with-icon'
-import { useForm } from 'figma-ui/src/hooks/use-form'
+import { Button } from '@create-figma-plugin/ui/src/components/button/button'
+import { Container } from '@create-figma-plugin/ui/src/components/container/container'
+import { Header } from '@create-figma-plugin/ui/src/components/header/header'
+import { TextboxNumeric } from '@create-figma-plugin/ui/src/components/textbox/textbox-numeric/textbox-numeric'
+import { useForm } from '@create-figma-plugin/ui/src/hooks/use-form'
 import { h } from 'preact'
-import './draw-slice-over-selection.scss'
 
 export function DrawSliceOverSelection (initialState) {
   function submitCallback ({ padding }) {
@@ -21,24 +22,22 @@ export function DrawSliceOverSelection (initialState) {
     closeCallback
   )
   return (
-    <div class='draw-slice-over-selection'>
-      <div class='draw-slice-over-selection__label'>Set slice padding…</div>
-      <div class='draw-slice-over-selection__input'>
-        <InputWithIcon
-          type='number'
-          iconColor='black-3'
-          iconName='group'
-          name='padding'
-          onChange={handleInput}
-          value={inputs.padding}
-          focused
-        />
-      </div>
-      <div class='draw-slice-over-selection__button'>
-        <Button type='primary' onClick={handleSubmit}>
-          Draw Slice Over Selection
-        </Button>
-      </div>
-    </div>
+    <Container>
+      <Header>Padding</Header>
+      <TextboxNumeric
+        name='padding'
+        icon={
+          <svg width='10' height='10'>
+            <path d='M7 0H3v1h4V0zM1 1V0H0v1h1zM0 3h1v4H0V3zm9 0h1v4H9V3zm1-2H9V0h1v1zM3 9h4v1H3V9zM1 9H0v1h1V9zm8 1V9h1v1H9z' />
+          </svg>
+        }
+        onChange={handleInput}
+        value={inputs.padding}
+        focused
+      />
+      <Button fullWidth onClick={handleSubmit} style={{ marginTop: 12 }}>
+        Draw Slice Over Selection
+      </Button>
+    </Container>
   )
 }
