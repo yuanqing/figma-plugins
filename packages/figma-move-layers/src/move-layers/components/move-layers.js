0ca48210b13222aaa61002f46f7c811715297915
yuanqing <desk@yuanqing.sg>
2019-11-18 01:48:06 +0800

    Migrate to `@create-figma-plugin/ui`

--- packages/figma-move-layers/src/move-layers/components/move-layers.js
+++ packages/figma-move-layers/src/move-layers/components/move-layers.js
@@ -1,10 +1,10 @@
 /** @jsx h */
+import { Button } from '@create-figma-plugin/ui/src/components/button/button'
+import { Container } from '@create-figma-plugin/ui/src/components/container/container'
+import { TextboxNumeric } from '@create-figma-plugin/ui/src/components/textbox/textbox-numeric/textbox-numeric'
+import { useForm } from '@create-figma-plugin/ui/src/hooks/use-form'
 import { triggerEvent } from '@create-figma-plugin/utilities'
-import { Button } from 'figma-ui/src/components/button'
-import { InputWithIcon } from 'figma-ui/src/components/input-with-icon'
-import { useForm } from 'figma-ui/src/hooks/use-form'
 import { h } from 'preact'
-import './move-layers.scss'
 
 export function MoveLayers (initialState) {
   function submitCallback ({ horizontalOffset, verticalOffset }) {
@@ -22,33 +22,41 @@ export function MoveLayers (initialState) {
     closeCallback
   )
   return (
-    <div class='move-layers'>
-      <div class='move-layers__input'>
-        <InputWithIcon
-          type='number'
-          iconColor='black-3'
-          iconName='arrow-left-right'
-          name='horizontalOffset'
-          onChange={handleInput}
-          value={inputs.horizontalOffset}
-          focused
-        />
-      </div>
-      <div class='move-layers__input'>
-        <InputWithIcon
-          type='number'
-          iconColor='black-3'
-          iconName='arrow-up-down'
-          name='verticalOffset'
-          onChange={handleInput}
-          value={inputs.verticalOffset}
-        />
-      </div>
-      <div class='move-layers__button'>
-        <Button type='primary' onClick={handleSubmit}>
-          Move Selected Layers
-        </Button>
-      </div>
-    </div>
+    <Container>
+      <TextboxNumeric
+        name='horizontalOffset'
+        icon={
+          <svg width='12' height='6'>
+            <path
+              fill-rule='evenodd'
+              clip-rule='evenodd'
+              d='M2.207 3.5l1.647 1.646-.707.708-2.5-2.5L.293 3l.354-.354 2.5-2.5.707.708L2.207 2.5h7.586L8.147.854l.707-.708 2.5 2.5.353.354-.353.354-2.5 2.5-.707-.708L9.793 3.5H2.207z'
+            />
+          </svg>
+        }
+        onChange={handleInput}
+        value={inputs.horizontalOffset}
+        style={{ marginTop: 12 }}
+        focused
+      />
+      <TextboxNumeric
+        name='verticalOffset'
+        icon={
+          <svg width='6' height='12'>
+            <path
+              fill-rule='evenodd'
+              clip-rule='evenodd'
+              d='M3 .292l.354.354 2.5 2.5-.707.707-1.646-1.646v7.585l1.646-1.646.707.707-2.5 2.5-.353.354-.354-.354-2.5-2.5.707-.707 1.647 1.646V2.207L.854 3.853l-.707-.707 2.5-2.5.354-.354z'
+            />
+          </svg>
+        }
+        onChange={handleInput}
+        value={inputs.verticalOffset}
+        style={{ marginTop: 12 }}
+      />
+      <Button fullWidth onClick={handleSubmit} style={{ marginTop: 12 }}>
+        Move Selected Layers
+      </Button>
+    </Container>
   )
 }
