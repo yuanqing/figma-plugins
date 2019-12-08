25560753f89109eae9f2b50b8e1cc903be34cdc7
yuanqing <desk@yuanqing.sg>
2019-12-08 17:14:37 +0800

    Move icons to `@create-figma-plugin/ui`

--- packages/figma-distribute-layers/src/distribute-layers-right/ui.js
+++ packages/figma-distribute-layers/src/distribute-layers-right/ui.js
@@ -1,16 +1,7 @@
-/** @jsx h */
-import { h } from 'preact'
+import { spaceHorizontalIcon } from '@create-figma-plugin/ui'
 import { uiFactory } from '../ui-factory'
 
 export default uiFactory({
   direction: 'Right',
-  icon: (
-    <svg>
-      <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
-        d='M2 3H4V13H2V14H4.5H5V13.5V2.5V2H4.5H2V3ZM12 2H12.5H15V3H13V13H15V14H12.5H12V13.5V2.5V2ZM8 11V5H9V11H8Z'
-      />
-    </svg>
-  )
+  icon: spaceHorizontalIcon
 })
