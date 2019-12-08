098dffbf87943c8209358ad2c5fade16121d7963
yuanqing <desk@yuanqing.sg>
2019-12-08 17:14:51 +0800

    Move icons to `@create-figma-plugin/ui`

--- packages/figma-move-layers/src/move-layers/components/move-layers.js
+++ packages/figma-move-layers/src/move-layers/components/move-layers.js
@@ -5,6 +5,8 @@ import {
   Container,
   TextboxNumeric,
   VerticalSpace,
+  moveDownIcon,
+  moveRightIcon,
   useForm
 } from '@create-figma-plugin/ui'
 import { addEventListener, triggerEvent } from '@create-figma-plugin/utilities'
@@ -39,30 +41,14 @@ export function MoveLayers (initialState) {
       <Columns space='extraSmall'>
         <TextboxNumeric
           name='horizontalOffset'
-          icon={
-            <svg>
-              <path
-                fill-rule='evenodd'
-                clip-rule='evenodd'
-                d='M3 8.5V12H4L4 8.5H11.7929L9.64645 10.6464L10.3536 11.3536L13.3536 8.35355L13.7071 8L13.3536 7.64645L10.3536 4.64645L9.64645 5.35355L11.7929 7.5H4L4 4H3V7.5V8.5Z'
-              />
-            </svg>
-          }
+          icon={moveRightIcon}
           onChange={handleInput}
           value={inputs.horizontalOffset}
           focused
         />
         <TextboxNumeric
           name='verticalOffset'
-          icon={
-            <svg>
-              <path
-                fill-rule='evenodd'
-                clip-rule='evenodd'
-                d='M7.5 3H4V4L7.5 4L7.5 11.7929L5.35355 9.64645L4.64645 10.3536L7.64645 13.3536L8 13.7071L8.35355 13.3536L11.3536 10.3536L10.6464 9.64645L8.5 11.7929L8.5 4L12 4V3H8.5H7.5Z'
-              />
-            </svg>
-          }
+          icon={moveDownIcon}
           onChange={handleInput}
           value={inputs.verticalOffset}
         />
