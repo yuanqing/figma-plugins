12795b7bec6b69b99fb7880975ed63aac72294e9
yuanqing <desk@yuanqing.sg>
2019-12-08 17:15:01 +0800

    Move icons to `@create-figma-plugin/ui`

--- packages/figma-organize-layers/src/organize-layers/components/organize-layers.js
+++ packages/figma-organize-layers/src/organize-layers/components/organize-layers.js
@@ -7,6 +7,8 @@ import {
   Text,
   TextboxNumeric,
   VerticalSpace,
+  spaceHorizontalIcon,
+  spaceVerticalIcon,
   useForm
 } from '@create-figma-plugin/ui'
 import { addEventListener, triggerEvent } from '@create-figma-plugin/utilities'
@@ -80,29 +82,13 @@ export function OrganizeLayers (initialState) {
         <Columns space='extraSmall'>
           <TextboxNumeric
             name='horizontalSpace'
-            icon={
-              <svg>
-                <path
-                  fill-rule='evenodd'
-                  clip-rule='evenodd'
-                  d='M2 3H4V13H2V14H4.5H5V13.5V2.5V2H4.5H2V3ZM12 2H12.5H15V3H13V13H15V14H12.5H12V13.5V2.5V2ZM8 11V5H9V11H8Z'
-                />
-              </svg>
-            }
+            icon={spaceHorizontalIcon}
             onChange={handleInput}
             value={inputs.horizontalSpace}
           />
           <TextboxNumeric
             name='verticalSpace'
-            icon={
-              <svg>
-                <path
-                  fill-rule='evenodd'
-                  clip-rule='evenodd'
-                  d='M2 2V4.5V5H2.5H13.5H14V4.5V2H13V4H3V2H2ZM11 9H5V8H11V9ZM2 12H2.5H13.5H14V12.5V15H13V13H3V15H2V12.5V12Z'
-                />
-              </svg>
-            }
+            icon={spaceVerticalIcon}
             onChange={handleInput}
             value={inputs.verticalSpace}
           />
