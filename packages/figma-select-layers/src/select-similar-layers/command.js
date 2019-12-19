19cd779c3e939e56f9d3078470b5f79399db3066
yuanqing <desk@yuanqing.sg>
2019-12-20 01:12:02 +0800

    Add select similar layers

---
+++ packages/figma-select-layers/src/select-similar-layers/command.js
@@ -0,0 +1,37 @@
+/* global figma */
+import {
+  formatErrorMessage,
+  formatSuccessMessage,
+  mapNumberToWord,
+  pluralize
+} from '@create-figma-plugin/utilities'
+import { getSimilarLayers } from './get-similar-layers'
+
+export default function () {
+  const length = figma.currentPage.selection.length
+  if (length !== 1) {
+    figma.closePlugin(
+      formatErrorMessage(
+        length === 0
+          ? 'Select a reference layer'
+          : 'Select only one reference layer'
+      )
+    )
+    return
+  }
+  const referenceLayer = figma.currentPage.selection[0]
+  const layers = getSimilarLayers(referenceLayer)
+  if (layers.length === 0) {
+    figma.closePlugin(formatErrorMessage('No similar layers on page'))
+    return
+  }
+  figma.currentPage.selection = [referenceLayer, ...layers]
+  figma.closePlugin(
+    formatSuccessMessage(
+      `Selected ${mapNumberToWord(layers.length)} ${pluralize(
+        layers.length,
+        'similar layer'
+      )}`
+    )
+  )
+}
