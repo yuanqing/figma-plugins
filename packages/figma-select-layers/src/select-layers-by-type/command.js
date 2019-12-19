15c07fa19090076fc26dc187a4cbdc361edf8844
yuanqing <desk@yuanqing.sg>
2019-12-19 22:00:22 +0800

    Add select by type

---
+++ packages/figma-select-layers/src/select-layers-by-type/command.js
@@ -0,0 +1,66 @@
+/* global figma */
+import {
+  formatErrorMessage,
+  formatSuccessMessage,
+  getSelectedLayersOrAllLayers,
+  mapNumberToWord,
+  pluralize
+} from '@create-figma-plugin/utilities'
+
+export const component = commandFactory(
+  filterByTypeFactory('COMPONENT'),
+  'component'
+)
+export const instance = commandFactory(
+  filterByTypeFactory('INSTANCE'),
+  'instance'
+)
+export const frame = commandFactory(filterByTypeFactory('FRAME'), 'frame')
+export const group = commandFactory(filterByTypeFactory('GROUP'), 'group')
+export const slice = commandFactory(filterByTypeFactory('SLICE'), 'slice')
+export const rectangle = commandFactory(
+  filterByTypeFactory('RECTANGLE'),
+  'rectangle'
+)
+export const line = commandFactory(filterByTypeFactory('LINE'), 'line')
+export const ellipse = commandFactory(filterByTypeFactory('ELLIPSE'), 'ellipse')
+export const polygon = commandFactory(filterByTypeFactory('POLYGON'), 'polygon')
+export const star = commandFactory(filterByTypeFactory('STAR'), 'star')
+export const vector = commandFactory(filterByTypeFactory('VECTOR'), 'vector')
+export const image = commandFactory(filterImages, 'image')
+export const text = commandFactory(filterByTypeFactory('TEXT'), 'text')
+
+function commandFactory (filterCallback, label) {
+  return function () {
+    const layers = getSelectedLayersOrAllLayers().filter(filterCallback)
+    const scope =
+      figma.currentPage.selection.length > 0 ? 'within selection' : 'on page'
+    figma.currentPage.selection = layers
+    /* eslint-disable indent */
+    figma.closePlugin(
+      layers.length === 0
+        ? formatErrorMessage(`No ${label}s ${scope}`)
+        : formatSuccessMessage(
+            `Selected ${mapNumberToWord(layers.length)} ${pluralize(
+              layers.length,
+              label
+            )} ${scope}`
+          )
+    )
+    /* eslint-enable indent */
+  }
+}
+
+function filterByTypeFactory (type) {
+  return function (layer) {
+    return layer.type === type
+  }
+}
+
+function filterImages (layer) {
+  return (
+    layer.type === 'RECTANGLE' &&
+    layer.fills.length === 1 &&
+    layer.fills[0].type === 'IMAGE'
+  )
+}
