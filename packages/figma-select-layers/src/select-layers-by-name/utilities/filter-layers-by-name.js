f6a2ccb9b0b2488fbedb29b7d4d99734584aea9f
yuanqing <desk@yuanqing.sg>
2020-01-16 23:20:48 +0800

    Traverse

--- packages/figma-select-layers/src/select-layers-by-name/utilities/filter-layers-by-name.js
+++ packages/figma-select-layers/src/select-layers-by-name/utilities/filter-layers-by-name.js
@@ -1,3 +1,5 @@
+import { traverseLayer } from '@create-figma-plugin/utilities'
+
 export function filterLayersByName (layers, layerName, exactMatch) {
   if (layerName === '') {
     return []
@@ -5,16 +7,20 @@ export function filterLayersByName (layers, layerName, exactMatch) {
   const result = []
   if (exactMatch === true) {
     for (const layer of layers) {
-      if (layer.name === layerName) {
-        result.push(layer)
-      }
+      traverseLayer(layer, function (layer) {
+        if (layer.name === layerName) {
+          result.push(layer)
+        }
+      })
     }
   } else {
     const regex = new RegExp(layerName, 'i')
     for (const layer of layers) {
-      if (regex.test(layer.name) === true) {
-        result.push(layer)
-      }
+      traverseLayer(layer, function (layer) {
+        if (regex.test(layer.name) === true) {
+          result.push(layer)
+        }
+      })
     }
   }
   return result
