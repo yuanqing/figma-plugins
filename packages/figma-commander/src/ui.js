a8f1e145150b65705c1ddf3573f705af20510034
yuanqing <desk@yuanqing.sg>
2020-01-27 10:27:55 +0800

    rm -rf packages/figma-commander

--- packages/figma-commander/src/ui.js
+++
@@ -1,4 +0,0 @@
-import { render } from '@create-figma-plugin/ui'
-import { Commander } from './components/commander'
-
-export default render(Commander)
--- packages/figma-commander/src/utilities/argument-types.js
+++
@@ -1,2 +0,0 @@
-export const NUMBER = 'NUMBER'
-export const STRING = 'STRING'
