046097133f41ed8cc93b4aa507ec12d2c56e55de
yuanqing <desk@yuanqing.sg>
2020-01-27 23:19:19 +0800

    Fix typo

--- packages/figma-select-layers/src/select-layers-by-name/command.js
+++ packages/figma-select-layers/src/select-layers-by-name/command.js
@@ -50,7 +50,7 @@ export default async function () {
   })
   const { layerName, exactMatch } = settings.selectLayersByName
   showUI(
-    { width: 240, height: 168 },
+    { width: 240, height: 164 },
     {
       layerName,
       exactMatch,
