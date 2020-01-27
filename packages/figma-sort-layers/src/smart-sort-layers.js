1d6c931e3ce7520a7a07461bd1a96c9548af1e40
yuanqing <desk@yuanqing.sg>
2020-01-27 10:26:08 +0800

    Account for empty `layers`

--- packages/figma-sort-layers/src/smart-sort-layers.js
+++ packages/figma-sort-layers/src/smart-sort-layers.js
@@ -1,6 +1,9 @@
 import { smartSortChildLayers } from './smart-sort-child-layers'
 
 export function smartSortLayers (layers) {
+  if (layers.length === 0) {
+    return []
+  }
   const parentLayer = layers[0].parent
   const layerIds = collectLayerIds(layers)
   return smartSortChildLayers(parentLayer, layerIds)
