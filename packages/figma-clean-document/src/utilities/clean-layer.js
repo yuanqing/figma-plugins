2142b0faa575b08495a152b85faaef83cd1b325d
yuanqing <desk@yuanqing.sg>
2020-01-29 21:44:00 +0800

    Refactor

--- packages/figma-clean-document/src/utilities/clean-layer.js
+++ packages/figma-clean-document/src/utilities/clean-layer.js
@@ -3,64 +3,45 @@ import { makePixelPerfect } from './make-pixel-perfect'
 import { smartRenameLayer } from './smart-rename-layer'
 import { ungroupSingleLayerGroup } from './ungroup-single-layer-group'
 
-export function cleanLayer (
-  layer,
-  {
+export function cleanLayer (layer, settings) {
+  const {
     deleteHiddenLayers,
     pixelPerfect,
     smartRenameLayers,
     smartRenameLayersWhitelistRegex,
     ungroupSingleLayerGroups
-  }
-) {
+  } = settings
   if (layer.removed === true) {
     return false
   }
-  if (deleteHiddenLayers === true && deleteHiddenLayer(layer) === true) {
-    return true
-  }
   let didChange = false
   if (typeof layer.children !== 'undefined') {
-    const settings = {
-      deleteHiddenLayers,
-      pixelPerfect,
-      smartRenameLayers,
-      smartRenameLayersWhitelistRegex,
-      ungroupSingleLayerGroups
-    }
-    const isInstance = layer.type === 'INSTANCE'
-    if (isInstance || layer.type === 'COMPONENT') {
-      settings.deleteHiddenLayers = false
-    }
-    if (isInstance || layer.type === 'VECTOR') {
-      settings.pixelPerfect = false
-    }
-    if (isInstance) {
-      settings.ungroupSingleLayerGroups = false
-    }
-    if (
-      settings.deleteHiddenLayers === true ||
-      settings.pixelPerfect === true ||
-      settings.smartRenameLayers === true ||
-      settings.smartRenameLayersWhitelistRegex === true ||
-      settings.ungroupSingleLayerGroups === true
-    ) {
+    if (layer.type === 'INSTANCE') {
+      if (smartRenameLayers === true) {
+        for (const childLayer of layer.children) {
+          didChange =
+            smartRenameLayer(childLayer, smartRenameLayersWhitelistRegex) ||
+            didChange
+        }
+      }
+    } else {
       for (const childLayer of layer.children) {
         didChange = cleanLayer(childLayer, settings) || didChange
       }
     }
   }
+  if (deleteHiddenLayers === true) {
+    didChange = deleteHiddenLayer(layer) || didChange
+  }
   if (ungroupSingleLayerGroups === true) {
-    if (ungroupSingleLayerGroup(layer) === true) {
-      return true
-    }
+    didChange = ungroupSingleLayerGroup(layer) || didChange
+  }
+  if (pixelPerfect === true) {
+    didChange = makePixelPerfect(layer) || didChange
   }
   if (smartRenameLayers === true) {
     didChange =
       smartRenameLayer(layer, smartRenameLayersWhitelistRegex) || didChange
-  }
-  if (pixelPerfect === true) {
-    didChange = makePixelPerfect(layer) || didChange
   }
   return didChange
 }
