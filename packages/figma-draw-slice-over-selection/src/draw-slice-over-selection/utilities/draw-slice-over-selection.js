08db9a41727f074100d0ef80a08682232e3e9116
yuanqing <desk@yuanqing.sg>
2020-02-03 21:07:03 +0800

    Use `computeMaximumBounds` from `@create-figma-plugin/utilities`

--- packages/figma-draw-slice-over-selection/src/draw-slice-over-selection/utilities/draw-slice-over-selection.js
+++ packages/figma-draw-slice-over-selection/src/draw-slice-over-selection/utilities/draw-slice-over-selection.js
@@ -1,7 +1,7 @@
-import { calculateMaximumBounds } from './calculate-maximum-bounds'
+import { computeMaximumBounds } from '@create-figma-plugin/utilities'
 
 export function drawSliceOverSelection (padding) {
-  const maximumBounds = calculateMaximumBounds(figma.currentPage.selection)
+  const maximumBounds = computeMaximumBounds(figma.currentPage.selection)
   const slice = figma.createSlice()
   const width = maximumBounds[1].x - maximumBounds[0].x + 2 * padding
   const height = maximumBounds[1].y - maximumBounds[0].y + 2 * padding
