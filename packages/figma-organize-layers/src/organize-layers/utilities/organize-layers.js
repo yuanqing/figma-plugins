2f84f962408e8574f1300e908f678aec16bb0e11
yuanqing <desk@yuanqing.sg>
2020-02-14 14:14:43 +0800

    Fix sort order

--- packages/figma-organize-layers/src/organize-layers/utilities/organize-layers.js
+++ packages/figma-organize-layers/src/organize-layers/utilities/organize-layers.js
@@ -1,6 +1,5 @@
 import {
   collapseLayer,
-  sortLayersByName,
   updateLayersSortOrder
 } from '@create-figma-plugin/utilities'
 import { arrangeGroups } from './arrange-groups'
@@ -15,9 +14,22 @@ export function organizeLayers (
 ) {
   const groups = groupLayers(layers, combineSingleLayerGroups, groupDefinition)
   arrangeGroups(groups, horizontalSpace, verticalSpace)
+  updateSortOrder(groups)
+  collapseLayers(layers)
+}
+
+function updateSortOrder (groups) {
+  const layers = []
+  for (const group of groups) {
+    for (const layer of group.layers) {
+      layers.push(layer)
+    }
+  }
+  updateLayersSortOrder(layers)
+}
+
+function collapseLayers (layers) {
   for (const layer of layers) {
     collapseLayer(layer)
   }
-  const result = sortLayersByName(layers)
-  updateLayersSortOrder(result)
 }
