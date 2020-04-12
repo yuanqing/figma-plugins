fdbdda194370e6d7092abe90a0445ed6a995a481
yuanqing <desk@yuanqing.sg>
2020-04-12 10:37:05 +0800

    Use `createImagePaint`

--- packages/figma-insert-big-image/src/insert-big-image/utilities/create-image-layer.js
+++ packages/figma-insert-big-image/src/insert-big-image/utilities/create-image-layer.js
@@ -1,3 +1,5 @@
+import { createImagePaint } from '@create-figma-plugin/utilities'
+
 export function createImageLayer (
   [bytes, x, y, width, height],
   xOffset,
@@ -12,30 +14,6 @@ export function createImageLayer (
     insertAs2x === true ? width / 2 : width,
     insertAs2x === true ? height / 2 : height
   )
-  const image = figma.createImage(bytes)
-  rectangle.fills = [
-    {
-      type: 'IMAGE',
-      visible: true,
-      opacity: 1,
-      blendMode: 'NORMAL',
-      scaleMode: 'FILL',
-      imageTransform: [
-        [1, 0, 0],
-        [0, 1, 0]
-      ],
-      scalingFactor: 0.5,
-      filters: {
-        exposure: 0,
-        contrast: 0,
-        saturation: 0,
-        temperature: 0,
-        tint: 0,
-        highlights: 0,
-        shadows: 0
-      },
-      imageHash: image.hash
-    }
-  ]
+  rectangle.fills = [createImagePaint(bytes)]
   return rectangle
 }
