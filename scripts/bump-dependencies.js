587b23b3badddbf42a350ed5d498dc3f20f847fa
yuanqing <desk@yuanqing.sg>
2020-05-02 13:34:52 +0800

    Add `bump-dependencies` script

---
+++ scripts/bump-dependencies.js
@@ -0,0 +1,25 @@
+const globby = require('globby')
+const ncu = require('npm-check-updates')
+const path = require('path')
+
+async function main () {
+  const parentDirectory = path.resolve(__dirname, '..')
+  const globs = [
+    path.join(parentDirectory, 'package.json'),
+    path.join(parentDirectory, 'packages', '**', 'package.json')
+  ]
+  const packageJsonFiles = await globby(globs, { deep: 2 })
+  const promises = []
+  packageJsonFiles.forEach(function (packageJsonFile) {
+    promises.push(
+      ncu.run({
+        packageFile: packageJsonFile,
+        packageManager: 'npm',
+        silent: false,
+        upgrade: true
+      })
+    )
+  })
+  await Promise.all(promises)
+}
+main()
