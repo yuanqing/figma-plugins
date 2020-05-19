353c1b079aa4a51b71fe7e090107389c99fa7758
yuanqing <desk@yuanqing.sg>
2020-05-20 00:19:08 +0800

    Use sort page function from `figma-sort-layers`

---
+++ packages/figma-clean-document/src/sort-pages-by-name/main.ts
@@ -0,0 +1,13 @@
+import { formatSuccessMessage } from '@create-figma-plugin/utilities'
+import { sortPagesByName } from 'figma-sort-layers/src/sort-pages-by-name/utilities/sort-pages-by-name'
+import { updatePagesSortOrder } from 'figma-sort-layers/src/sort-pages-by-name/utilities/update-pages-sort-order'
+
+export default function () {
+  const result = sortPagesByName()
+  const didChange = updatePagesSortOrder(result)
+  figma.closePlugin(
+    didChange === true
+      ? formatSuccessMessage('Sorted pages by name')
+      : 'No change to page sort order'
+  )
+}
