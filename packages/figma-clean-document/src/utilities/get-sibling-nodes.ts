659f2deb5de2f05ad10bf72a7cf78d4e239edd84
yuanqing <desk@yuanqing.sg>
2020-08-15 10:42:54 +0800

    Remove `.reverse()`

--- packages/figma-clean-document/src/utilities/get-sibling-nodes.ts
+++ packages/figma-clean-document/src/utilities/get-sibling-nodes.ts
@@ -3,7 +3,7 @@ import { computeSiblingNodes } from '@create-figma-plugin/utilities'
 export function getSiblingNodes(): Array<Array<SceneNode>> {
   const selection = figma.currentPage.selection
   if (selection.length === 0) {
-    return [figma.currentPage.children.slice().reverse()]
+    return [figma.currentPage.children.slice()]
   }
   if (selection.length === 1 && 'children' in selection[0]) {
     return computeSiblingNodes([selection[0], ...selection[0].children])
