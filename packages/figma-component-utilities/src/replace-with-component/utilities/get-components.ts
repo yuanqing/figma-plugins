79d5bc597e3320468587dfdacead827b9a3f9746
yuanqing <desk@yuanqing.sg>
2021-04-15 09:05:18 +0800

    Update `extractAttributes`

--- packages/figma-component-utilities/src/replace-with-component/utilities/get-components.ts
+++ packages/figma-component-utilities/src/replace-with-component/utilities/get-components.ts
@@ -8,8 +8,5 @@ import { ComponentNodeAttributes } from '../types'
 
 export function getComponents(): Array<ComponentNodeAttributes> {
   const components = sortNodesByName(getDocumentComponents())
-  return extractAttributes(components, [
-    'id',
-    'name'
-  ]) as Array<ComponentNodeAttributes>
+  return extractAttributes<ComponentNodeAttributes>(components, ['id', 'name'])
 }
