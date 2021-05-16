f06decbb9640698b329c3f9157bfe07a84216636
yuanqing <desk@yuanqing.sg>
2021-05-16 21:25:55 +0800

    Edit `bump-dependencies` script

--- scripts/bump-dependencies.ts
+++ scripts/bump-dependencies.ts
@@ -1,26 +1,43 @@
 import globby from 'globby'
-import ncu from 'npm-check-updates'
-import * as path from 'path'
+import ncu, { RunResults } from 'npm-check-updates'
+import { dirname, join, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 async function main(): Promise<void> {
-  const parentDirectory = path.resolve(__dirname, '..')
-  const globs = [
-    path.join(parentDirectory, 'package.json'),
-    path.join(parentDirectory, 'packages', '**', 'package.json')
-  ]
-  const packageJsonFiles = await globby(globs, { deep: 2 })
-  const promises: Array<Promise<void>> = []
-  packageJsonFiles.forEach(function (packageJsonFile) {
-    async function run() {
-      await ncu.run({
-        packageFile: packageJsonFile,
+  try {
+    const parentDirectoryPath = resolve(__dirname, '..')
+    const globPatterns = [
+      join(parentDirectoryPath, 'package.json'),
+      join(parentDirectoryPath, 'packages', '**', 'package.json')
+    ]
+    await bumpDependencies(globPatterns)
+  } catch (error) {
+    console.error(error.message) // eslint-disable-line no-console
+    process.exit(1)
+  }
+}
+main()
+
+async function bumpDependencies(globPatterns: Array<string>): Promise<void> {
+  const args = process.argv.slice(2) // Modules passed in as CLI arguments will _not_ be bumped
+  const packageJsonFilePaths = await globby(globPatterns, {
+    deep: 2
+  })
+  const promises: Array<Promise<RunResults>> = []
+  for (const filePath of packageJsonFilePaths) {
+    promises.push(
+      ncu.run({
+        packageFile: filePath,
         packageManager: 'npm',
+        reject: args,
         silent: false,
+        target: 'latest',
         upgrade: true
       })
-    }
-    promises.push(run())
-  })
+    )
+  }
   await Promise.all(promises)
 }
 main()
