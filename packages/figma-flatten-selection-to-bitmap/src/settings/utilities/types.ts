4ed5bf24110fdd35aacc1d8ac9f29c04e989a826
Yuan Qing Lim <desk@yuanqing.sg>
2022-08-09 17:12:33 +0800

    Revert UI

---
+++ packages/figma-flatten-selection-to-bitmap/src/settings/utilities/types.ts
@@ -0,0 +1,15 @@
+import { EventHandler } from '@create-figma-plugin/utilities'
+
+import { Settings } from '../../utilities/types.js'
+
+export type SettingsProps = Settings
+export type FormState = Settings
+
+export interface CloseUIHandler extends EventHandler {
+  name: 'CLOSE_UI'
+  handler: () => void
+}
+export interface SubmitHandler extends EventHandler {
+  name: 'SUBMIT'
+  handler: (settings: Settings) => void
+}
